91f06baf74afa28a752439751ce5048d3f705864
Tanisi90 <69205416+Tanisi90@users.noreply.github.com>
2023-11-08 13:55:46 -0700

    Update Expressions.java

--- BooleanOperators/src/main/java/Expressions.java
+++ BooleanOperators/src/main/java/Expressions.java
@@ -39,7 +39,7 @@ public class Expressions {
     /**
      * @param b1 a true/false value.
      * @param b2 a true/false value.
-     * @return true if either b1 or b1 are true. false if both are false.
+     * @return true if either b1 or b2 are true. false if both are false.
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
